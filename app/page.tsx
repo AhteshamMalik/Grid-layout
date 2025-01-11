097268de1d7dbf684cc5a224e0b6727b56161d0a
Ahtesham <161864884+AhteshamMalik@users.noreply.github.com>
2025-01-11 10:19:53 +0500

    Grid layout. By : Ahtesham Malik

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,41 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex h-[100vh] w-full justify-center items-center text-center">
+      <div className="main h-[550px] w-[850px] grid gap-1 grid-cols-5 bg-gradient p-5 rounded">
+        <div className="font-extrabold text-3xl bg-purple-500 row-span-10 text-center rounded">
+          AG1
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="font-extrabold text-3xl bg-yellow-300 col-span-3 rounded">
+          AG2
+        </div>
+        <div className="font-extrabold text-3xl bg-sky-500 row-span-10 rounded">
+          AG3
+        </div>
+        <div className="g4-g5 col-span-3 row-span-3 grid gap-7 grid-cols-2">
+          <div className="font-extrabold text-3xl bg-pink-400 rounded">AG4</div>
+          <div className="font-extrabold text-3xl bg-pink-400 rounded">AG5</div>
+        </div>
+        <div className="font-extrabold text-3xl bg-orange-500 row-span-6 rounded">
+          AG6
+        </div>
+        <div className="g7-g8-g9-g10 col-span-2 row-span-6 grid grid-cols-2">
+          <div className="font-extrabold text-3xl bg-yellow-500 col-span-2 rounded">
+            AG7
+          </div>
+          <div className="g8-g9 col-span-2 grid gap-5 grid-cols-2">
+            <div className="font-extrabold text-3xl bg-pink-400 rounded">
+              AG8
+            </div>
+            <div className="font-extrabold text-3xl bg-pink-400 rounded">
+              AG9
+            </div>
+          </div>
+          <div className="font-extrabold text-3xl bg-pink-600 col-span-2 rounded">
+            AG10
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
